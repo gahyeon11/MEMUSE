e75683c8c0d07b2d7716dd80a9586a40387f7ae0
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2023-11-12 15:51:32 +0900

    .

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DS\Documents\GitHub\MEMUSE\venv
+set VIRTUAL_ENV=C:\Users\keh56\OneDrive\문서\GitHub\MEMUSE\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
