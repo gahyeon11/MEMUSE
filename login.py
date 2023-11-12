aa30c31546510d817bfa461c97c2f0f2ac75a760
summete <144118364+summete@users.noreply.github.com>
2023-11-13 01:32:37 +0900

    stt 처리
    처리처리처리

--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template, url_for, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask import g 
 from flask_migrate import Migrate
 from sqlalchemy.exc import IntegrityError
 from PIL import Image, PngImagePlugin, ImageEnhance, ImageFilter
@@ -28,27 +29,27 @@ pymysql.install_as_MySQLdb()
 app = Flask(__name__)
 CORS(app)
 
-# db_path = 'translations.db'
+db_path = 'translations.db'
 
 # 절대 경로로 변환
-# absolute_path = os.path.abspath(db_path)
+absolute_path = os.path.abspath(db_path)
 
 # 데이터베이스 연결
 
-# conn = sqlite3.connect('translations.db', check_same_thread=False)
-# c = conn.cursor()
+conn = sqlite3.connect('translations.db', check_same_thread=False)
+c = conn.cursor()
 
 # # translations 테이블 생성 쿼리
-# c.execute('''
-#     CREATE TABLE IF NOT EXISTS translations (
-#         id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         original_text TEXT,
-#         translated_text TEXT
-#     )
-# ''')
+c.execute('''
+     CREATE TABLE IF NOT EXISTS translations (
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         original_text TEXT,
+         translated_text TEXT
+     )
+''')
 
 # # 변경 내용 저장
-# conn.commit()
+conn.commit()
 
 # # 연결 종료
 # # conn.close()
@@ -59,6 +60,55 @@ CORS(app)
 
 # conn = sqlite3.connect('users.db', check_same_thread=False)
 # c = conn.cursor()
+
+# 번역 함수
+def translate_text(text_to_translate):
+    try:
+        # Naver Papago API에 번역 요청
+        naver_client_id = 'fIfMjFH9VJ1GjQPim7j5'
+        naver_client_secret = 'iue1sb53T7'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Naver-Client-Id': naver_client_id,
+            'X-Naver-Client-Secret': naver_client_secret,
+        }
+        data = {
+            'source': 'ko',
+            'target': 'en',
+            'text': text_to_translate,
+        }
+        response = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
+        translated_text = response.json()['message']['result']['translatedText']
+        return translated_text
+    except Exception as e:
+        return str(e)
+
+# 번역 요청을 처리하는 엔드포인트
+@app.route('/translate', methods=['POST'])
+def translate_endpoint():
+    try:
+        request_data = request.json
+        text_to_translate = request_data['text']
+        translated_text = translate_text(text_to_translate)
+        return jsonify({'translatedText': translated_text})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# @app.route('/get_translation', methods=['GET'])
+# def get_translation():
+#     try:
+#         # 데이터베이스에서 데이터를 가져옴
+#         conn = sqlite3.connect(absolute_path, check_same_thread=False)
+#         c = conn.cursor()
+#         c.execute("SELECT * FROM your_table")
+#         data = c.fetchall()
+#         conn.close()
+
+#         # 가져온 데이터를 JSON 형식으로 응답
+#         response_data = [{"id": row[0], "text": row[1]} for row in data]
+#         return jsonify(response_data)
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 
 conn = sqlite3.connect('users.db', check_same_thread=False)
@@ -182,81 +232,72 @@ def make_shell_context():
 with app.app_context():
     db.create_all()
 
-@app.route('/translate', methods=['POST'])
-def translate():
-    try:
-        data = request.json
-        text = data.get('text')
-        # print(text)
+# @app.route('/translate', methods=['POST'])
+# def translate():
+#     try:
+#         data = request.json
+#         text = data.get('text')
 
-        if not text:
-            raise ValueError("No text provided")
+#         if not text:
+#             raise ValueError("No text provided")
 
-        client_id = 'fIfMjFH9VJ1GjQPim7j5'
-        client_secret = 'iue1sb53T7'
+#         client_id = 'fIfMjFH9VJ1GjQPim7j5'
+#         client_secret = 'iue1sb53T7'
 
-        url = 'https://openapi.naver.com/v1/papago/n2mt'
-        headers = {
-            'Content-Type': 'application/json',  # JSON 형식으로 설정
-            'X-Naver-Client-Id': client_id,
-            'X-Naver-Client-Secret': client_secret,
-        }
-        data = {
-            'source': 'ko',
-            'target': 'en',
-            'text': text
-        }
+#         url = 'https://openapi.naver.com/v1/papago/n2mt'
+#         headers = {
+#             'Content-Type': 'application/json',
+#             'X-Naver-Client-Id': client_id,
+#             'X-Naver-Client-Secret': client_secret,
+#         }
+#         data = {
+#             'source': 'ko',
+#             'target': 'en',
+#             'text': text
+#         }
        
-        response = requests.post(url, headers=headers, json=data)  # JSON 데이터로 요청
+#         response = requests.post(url, headers=headers, json=data)
 
-        if response.status_code != 200:
-            raise Exception("Translation API request failed")
+#         if response.status_code != 200:
+#             raise Exception("Translation API request failed")
 
-        translated_text = response.json().get('message', {}).get('result', {}).get('translatedText', "")
-        
-        c.execute("INSERT INTO translations (original_text, translated_text) VALUES (?, ?)", (text, translated_text))
-        conn.commit()
+#         translated_text = response.json().get('message', {}).get('result', {}).get('translatedText', "")
 
-        # g 객체를 사용하여 전역 변수 대신 저장
-        g.translated_text = translated_text
+#         c.execute("INSERT INTO translations (original_text, translated_text) VALUES (?, ?)", (text, translated_text))
+#         conn.commit()
 
-        return jsonify(status="success", message="서버 저장 성공",translated_text=translated_text)
+#         return jsonify(status="success", message="서버 저장 성공", translated_text=translated_text)
 
-    except ValueError as e:
-        logging.error(f"ValueError: {e}")
-        return jsonify(status="failure", error=str(e), message="서버 저장 실패"), 400
-    except Exception as e:
-        logging.error(f"Exception: {e}")
-        return jsonify(status="failure", error=str(e), message="서버 저장 실패"), 500
+#     except ValueError as e:
+#         logging.error(f"ValueError: {e}")
+#         return jsonify(status="failure", error=str(e), message="서버 저장 실패"), 400
+#     except Exception as e:
+#         logging.error(f"Exception: {e}")
+#         return jsonify(status="failure", error=str(e), message="서버 저장 실패"), 500
 
+# @app.route('/save_results', methods=['POST'])
+# def save_results():
+#     try:
+#         data = request.json
+#         original_text = data.get('originalText')
+#         translated_text = data.get('translatedText')
 
-@app.route('/save_results', methods=['POST'])
-def save_results():
-    try:
-        data = request.json
-        original_text = data.get('originalText')
-        translated_text = data.get('translatedText')
+#         c.execute("INSERT INTO translations (original_text, translated_text) VALUES (?, ?)", (original_text, translated_text))
+#         conn.commit()
 
-        # 데이터베이스에 저장
-        c.execute("INSERT INTO translations (original_text, translated_text) VALUES (?, ?)", (original_text, translated_text))
-        conn.commit()
+#         return jsonify(status="success", message="Result saved successfully")
+#     except Exception as e:
+#         logging.error(f"Exception: {e}")
+#         return jsonify(status="failure", error=str(e), message="Failed to save result"), 500
 
-        return jsonify(status="success", message="Result saved successfully")
-    except Exception as e:
-        logging.error(f"Exception: {e}")
-        return jsonify(status="failure", error=str(e), message="Failed to save result"), 500
-
-
-@app.route('/get_translation', methods=['GET'])
-def get_translation():
-    # g 객체를 사용하여 저장된 번역된 텍스트를 가져옴
-    translated_text = getattr(g, 'translated_text', None)
+# @app.route('/get_translation', methods=['GET'])
+# def get_translation():
+#     translated_text = getattr(g, 'translated_text', None)
     
-    if translated_text:
-        return jsonify(status="success", translated_text=translated_text)
-    else:
-        return jsonify(status="failure", message="저장된 번역이 없음"), 404
-
+#     if translated_text:
+#         return jsonify(status="success", translated_text=translated_text)
+#     else:
+#         return jsonify(status="failure", message="저장된 번역이 없음"), 404
 
 @app.route('/join', methods=['GET', 'POST'])
 def join():
@@ -734,26 +775,26 @@ def pro_back():
 
 @app.route('/pro_back_complete')
 def pro_back_complete():
-    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json = payload)
-    print(payload)
-    r = response.json()
-    # 이미지 저장, 텍스트 데이터를 이진 데이터로 디코딩
-    for i in r['images']:
-        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
-        # API 요청을 보내 이미지 정보 검색
-        png_payload = {
-            "image": "data:image/png;base64," + i
-        }
-        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json = png_payload)
-        # PIL 이미지에 메타 데이터 삽입
-        pnginfo = PngImagePlugin.PngInfo()
-        pnginfo.add_text("parameters", response2.json().get("info"))
-        # 현재 날짜와 시간을 문자열로 가져와 파일 이름으로 설정
-        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-        file_name = f'object/output_t2i{current_time}.png'
+    # response = requests.post(url=f'{url}/sdapi/v1/txt2img', json = payload)
+    # print(payload)
+    # r = response.json()
+    # # 이미지 저장, 텍스트 데이터를 이진 데이터로 디코딩
+    # for i in r['images']:
+    #     image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+    #     # API 요청을 보내 이미지 정보 검색
+    #     png_payload = {
+    #         "image": "data:image/png;base64," + i
+    #     }
+    #     response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json = png_payload)
+    #     # PIL 이미지에 메타 데이터 삽입
+    #     pnginfo = PngImagePlugin.PngInfo()
+    #     pnginfo.add_text("parameters", response2.json().get("info"))
+    #     # 현재 날짜와 시간을 문자열로 가져와 파일 이름으로 설정
+    #     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    #     file_name = f'object/output_t2i{current_time}.png'
         
-        # 이미지 저장
-        image.save(file_name, pnginfo = pnginfo)
+    #     # 이미지 저장
+    #     image.save(file_name, pnginfo = pnginfo)
     
     username = session.get('username', 'Guest')
     return render_template('pro_back_complete.html', username=username)
@@ -897,27 +938,27 @@ def pro_object():
 
 @app.route('/pro_object_complete')
 def pro_object_complete():
-# 이미지 생성 코드
-    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json = payload)
-    print(payload)
-    r = response.json()
-    # 이미지 저장, 텍스트 데이터를 이진 데이터로 디코딩
-    for i in r['images']:
-        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
-        # API 요청을 보내 이미지 정보 검색
-        png_payload = {
-            "image": "data:image/png;base64," + i
-        }
-        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json = png_payload)
-        # PIL 이미지에 메타 데이터 삽입
-        pnginfo = PngImagePlugin.PngInfo()
-        pnginfo.add_text("parameters", response2.json().get("info"))
-        # 현재 날짜와 시간을 문자열로 가져와 파일 이름으로 설정
-        current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-        file_name = f'object/output_t2i{current_time}.png'
+# # 이미지 생성 코드
+#     response = requests.post(url=f'{url}/sdapi/v1/txt2img', json = payload)
+#     print(payload)
+#     r = response.json()
+#     # 이미지 저장, 텍스트 데이터를 이진 데이터로 디코딩
+#     for i in r['images']:
+#         image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+#         # API 요청을 보내 이미지 정보 검색
+#         png_payload = {
+#             "image": "data:image/png;base64," + i
+#         }
+#         response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json = png_payload)
+#         # PIL 이미지에 메타 데이터 삽입
+#         pnginfo = PngImagePlugin.PngInfo()
+#         pnginfo.add_text("parameters", response2.json().get("info"))
+#         # 현재 날짜와 시간을 문자열로 가져와 파일 이름으로 설정
+#         current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+#         file_name = f'object/output_t2i{current_time}.png'
         
-        # 이미지 저장
-        image.save(file_name, pnginfo = pnginfo)
+#         # 이미지 저장
+#         image.save(file_name, pnginfo = pnginfo)
     username = session.get('username', 'Guest')
     return render_template('pro_object_complete.html', username=username)
 
